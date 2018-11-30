ac81cec45f04259c3ce037ba5d15c92fd53e3459
Cliff Li <cliff@lin.li>
2018-11-30 16:29:30 +0100

    Changes for flock_simulator

--- duckietown_visualization/src/publish_duckiebots.py
+++ duckietown_visualization/src/publish_duckiebots.py
@@ -1,8 +1,29 @@
-#!/usr/bin/env python  
+#!/usr/bin/env python
 import rospy
 import math
 import tf
 from visualization_msgs.msg import Marker, MarkerArray
+from flock_simulator.msg import FlockState
+
+
+def callback(bots):
+    marker_array = MarkerArray()
+    counter = 0
+
+    for it in bots.duckie_states:
+        duckie_id = it.duckie_id.data
+        try:
+            (trans,rot) = listener.lookupTransform('/duckiebot_link', \
+                duckie_id, rospy.Time(0))
+        except (tf.LookupException, tf.ConnectivityException,
+                tf.ExtrapolationException):
+            continue
+        marker_array.markers.append(
+            get_marker(counter, trans[0], trans[1], rot))
+        counter += 1
+
+    pub.publish(marker_array)
+
 
 def get_marker(marker_id, x, y, q):
     marker = Marker()
@@ -18,21 +39,22 @@ def get_marker(marker_id, x, y, q):
     marker.pose.position.y = y
     marker.pose.position.z = 0
 
-    marker.scale.x = 0.1
-    marker.scale.y = 0.2
+    marker.scale.x = 0.2
+    marker.scale.y = 0.1
     marker.scale.z = 0.1
 
     marker.color.r = 1
     marker.color.g = 1
     marker.color.b = 0
     marker.color.a = 1
-    
+
     marker.pose.orientation.x = q[0]
     marker.pose.orientation.y = q[1]
     marker.pose.orientation.z = q[2]
     marker.pose.orientation.w = q[3]
 
     return marker
+
 
 if __name__ == '__main__':
     rospy.init_node('duckiebot_marker_publisher')
@@ -43,19 +65,6 @@ if __name__ == '__main__':
     pub = rospy.Publisher('duckiebots_markers', MarkerArray, queue_size=10)
 
     rate = rospy.Rate(10.0)
-    while not rospy.is_shutdown():
-        marker_array = MarkerArray()
+    rospy.Subscriber('/flock_simulator/state', FlockState, callback)
 
-        for it in range(len(duckiebot_list)): 
-            try:
-                (trans,rot) = listener.lookupTransform('/duckiebot_link', \
-                    duckiebot_list[it], rospy.Time(0))
-            except (tf.LookupException, tf.ConnectivityException, 
-                tf.ExtrapolationException):
-                continue
-            marker_array.markers.append(get_marker(it,trans[0],trans[1],rot))
-        
-        pub.publish(marker_array)
-
-
-        rate.sleep()
+    rospy.spin()
--- visualization_example/src/publish_poses.py
+++ visualization_example/src/publish_poses.py
@@ -3,53 +3,28 @@ import rospy
 import tf
 import math
 import random
+from flock_simulator.msg import FlockState
 
 
-def get_random_radius(lower, upper):
-    return random.uniform(lower, upper)
+def callback(bots):
+    for it in bots.duckie_states:
+        t = rospy.Time.now()
+        theta = it.pose.theta
+        x = it.pose.x
+        y = it.pose.y
 
-
-def get_random_omega(lower, upper):
-    return random.uniform(lower, upper)
-
-
-def get_random_center(x_lower, x_upper, y_lower, y_upper):
-    return (random.uniform(x_lower, x_upper), random.uniform(x_lower, x_upper))
+        transform_broadcaster.sendTransform((x, y, 0), \
+            tf.transformations.quaternion_from_euler(0, 0, theta), \
+            t, it.duckie_id.data, "duckiebot_link")
 
 
 if __name__ == '__main__':
-
     rospy.init_node('duckiebot_pose_publisher', anonymous=False)
 
     rate = rospy.Rate(10)  # 10hz
 
     duckiebot_list = rospy.get_param('~duckiebot_list')
-
-    radius = []
-    omega = []
-    center = []
-
-    for it in range(len(duckiebot_list)):
-        # get a random radius in range [0.3,2] m
-        radius.append(get_random_radius(0.3, 2))
-        # get a random omega in range [0.1,0.5] rad/s
-        omega.append(get_random_omega(0.1, 0.5))
-        # get a random center of rotation with x in range [1,5] and y in [2,6]
-        center.append(get_random_center(1, 5, 2, 6))
-
     transform_broadcaster = tf.TransformBroadcaster()
+    rospy.Subscriber('/flock_simulator/state', FlockState, callback)
 
-    while not rospy.is_shutdown():
-
-        for it in range(len(duckiebot_list)):
-
-            t = rospy.Time.now()
-            theta = omega[it]*t.to_sec()
-            x = center[it][0] + radius[it]*math.cos(theta)
-            y = center[it][1] + radius[it]*math.sin(theta)
-
-            transform_broadcaster.sendTransform((x, y, 0), \
-                tf.transformations.quaternion_from_euler(0, 0, theta), \
-                t, duckiebot_list[it], "duckiebot_link")
-
-        rate.sleep()
+    rospy.spin()
